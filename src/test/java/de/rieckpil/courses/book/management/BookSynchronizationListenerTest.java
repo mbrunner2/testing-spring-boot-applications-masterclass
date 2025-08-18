a0791505aa2ff477d10475a99937e87d4610a713
MichaelBrunner <michael.brunner@ticketgretchen.com>
2025-08-18 15:11:49 +0200

    implement Tests for "Introduction to Mockito"

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -1,10 +1,13 @@
 package de.rieckpil.courses.book.management;
 
+import com.sun.jdi.connect.Connector;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,14 +24,62 @@ class BookSynchronizationListenerTest {
   @Captor private ArgumentCaptor<Book> bookArgumentCaptor;
 
   @Test
-  void shouldRejectBookWhenIsbnIsMalformed() {}
+  void shouldRejectBookWhenIsbnIsMalformed() {
+      BookSynchronization bookSynchronization = new BookSynchronization("42");
+
+      cut.consumeBookUpdates(bookSynchronization);
+
+      verifyNoInteractions(openLibraryApiClient, bookRepository);
+  }
 
   @Test
-  void shouldNotOverrideWhenBookAlreadyExists() {}
+  void shouldNotOverrideWhenBookAlreadyExists() {
+
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(new Book());
+
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verifyNoInteractions(openLibraryApiClient);
+    verify(bookRepository, times(0)).save(ArgumentMatchers.any());
+
+  }
 
   @Test
-  void shouldThrowExceptionWhenProcessingFails() {}
+  void shouldThrowExceptionWhenProcessingFails() {
+
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenThrow(new RuntimeException("Network timeout"));
+
+    assertThrows(RuntimeException.class, () -> cut.consumeBookUpdates(bookSynchronization));
+  }
 
   @Test
-  void shouldStoreBookWhenNewAndCorrectIsbn() {}
+  void shouldStoreBookWhenNewAndCorrectIsbn() {
+
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+
+    Book requestedBook = new Book();
+    requestedBook.setTitle("Bogus");
+    requestedBook.setIsbn(VALID_ISBN);
+
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenReturn(requestedBook);
+    when(bookRepository.save(ArgumentMatchers.any())).then(invocation -> {
+      Book methodArgument = invocation.getArgument(0);
+      methodArgument.setId(1L);
+      return methodArgument;
+    });
+
+    cut.consumeBookUpdates(bookSynchronization);
+
+    verify(bookRepository).save(bookArgumentCaptor.capture());
+
+    Book methodArgument = bookArgumentCaptor.getValue();
+    assertEquals("Bogus", methodArgument.getTitle());
+    assertEquals(VALID_ISBN, methodArgument.getIsbn());
+
+
+  }
 }
