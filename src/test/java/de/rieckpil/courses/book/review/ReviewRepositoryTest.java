6411d3644c56a255b7ceae127041b41494bbe7e2
MichaelBrunner <michael.brunner@ticketgretchen.com>
2025-08-19 13:01:06 +0200

    implement ReviewRepositoryTest

--- src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewRepositoryTest.java
@@ -1,15 +1,20 @@
 package de.rieckpil.courses.book.review;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 import javax.sql.DataSource;
 
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest(
     properties = {
@@ -29,9 +34,48 @@ class ReviewRepositoryTest {
 
   @Autowired private TestEntityManager testEntityManager;
 
-  @Test
-  void notNull() throws SQLException {}
+  @BeforeEach
+  void beforeEach() {
+    assertEquals(0,cut.count());
+  }
 
   @Test
-  void transactionalSupportTest() {}
+  void notNull() throws SQLException {
+    assertNotNull(entityManager);
+    assertNotNull(cut);
+    assertNotNull(testEntityManager);
+    assertNotNull(dataSource);
+
+    System.out.println(dataSource.getConnection().getMetaData().getDatabaseProductName());
+
+    Review review = new Review();
+    review.setTitle("Review XYZ");
+    review.setContent("Awesome review");
+    review.setCreatedAt(LocalDateTime.now());
+    review.setRating(5);
+    review.setBook(null);
+    review.setUser(null);
+
+    Review result = cut.save(review);
+    //Review result = testEntityManager.persistFlushFind(review);
+
+    assertNotNull(result.getId());
+  }
+
+  @Test
+  void transactionalSupportTest() {
+
+    Review review = new Review();
+    review.setTitle("Review XYZ");
+    review.setContent("Awesome review");
+    review.setCreatedAt(LocalDateTime.now());
+    review.setRating(5);
+    review.setBook(null);
+    review.setUser(null);
+
+    Review result = cut.save(review);
+
+    assertNotNull(result.getId());
+
+  }
 }
